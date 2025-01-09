804a031eb88c1294ebc45bc781835930e952e534
Jason Scheffel <contact@jasonscheffel.com>
2025-01-08 21:15:06 -0500

    feat: add check command logic for file existness
    Signed-off-by: Jason Scheffel <contact@jasonscheffel.com>

--- src/openbot/commands/check.py
+++ src/openbot/commands/check.py
@@ -1,9 +1,28 @@
 from openbot.platforms.base_platform import BasePlatform
 from openbot.commands.base_command import BaseCommand
 
+from pathlib import Path
+
+
 class Check(BaseCommand):
     def __init__(self, platform: BasePlatform) -> None:
         self.platform = platform
 
-    def run(self) -> None:
-        self.platform.send_message("running the check command")
+    def run(
+        self,
+        character_config: Path,
+        openbot_config: Path,
+    ) -> None:
+        if character_config.exists() and openbot_config.exists():
+            self.platform.send_message(
+                f"character config {character_config} and openbot config {openbot_config} exist"
+            )
+        else:
+            if not character_config.exists():
+                self.platform.send_message(
+                    f"character config {character_config} does not exist"
+                )
+            if not openbot_config.exists():
+                self.platform.send_message(
+                    f"openbot config {openbot_config} does not exist"
+                )
